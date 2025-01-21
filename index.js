c256dcafb1d12b8f50e62f0feed88a6ce1dfa327
Anatoliy Shchepetkov <tolick.shepetkov61@gmail.com>
2025-01-21 13:04:59 +0300

    tg-bot-2

--- index.js
+++ index.js
@@ -14,41 +14,41 @@ app.use(express.json());
 app.use(cors());
 
 bot.on('message', async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-  if(text === '/start') {
-    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-        reply_markup: {
-            keyboard: [
-                [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-            ]
-        }
-    })
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
 
-    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-            ]
-        }
-    })
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
     }
 
     if(msg?.web_app_data?.data) {
-    try {
-        const data = JSON.parse(msg?.web_app_data?.data)
-        console.log(data)
-        await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-        await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
-        await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
 
-        setTimeout(async () => {
-            await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-        }, 3000)
-    } catch (e) {
-        console.log(e);
-    }
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
     }
 });
 
